70c89d447444195d1a014939028c6ac6cc903bf1
Dmitiy <58909202+insert4079@users.noreply.github.com>
2021-02-05 14:39:58 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,15 +2,6 @@ let card = document.querySelectorAll('.card'),
     cardWrap = document.querySelectorAll('.cardWrap'),
     wrapper = document.querySelector('.wrapper');
 
-// cardWrap[0].addEventListener('mousemove', function(e) {
-//     card[0].style.transform = `rotateY(${(e.offsetX - card[0].clientWidth/2)/4}deg) rotateX(${-(e.offsetY - card[0].clientHeight/2)/4}deg)`
-// });
-
-
-
-
-
-
 for (let i = 0; i < card.length; i++) {
     cardWrap[i].addEventListener('mousemove', function(e) {
         card[i].style.transform = `rotateY(${(e.offsetX - card[i].clientWidth/2)/5}deg) rotateX(${-(e.offsetY - card[i].clientHeight/2)/5}deg)`
